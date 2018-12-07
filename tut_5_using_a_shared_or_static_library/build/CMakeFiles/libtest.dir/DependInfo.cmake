0b6d9eda6ea1cc1e6b2009e279f223886b490dca
nknight <knguyentnh98@gmail.com>
2018-12-07 21:25:28 +0700

    up tut

---
+++ tut_5_using_a_shared_or_static_library/build/CMakeFiles/libtest.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nknight/Works/git/cmake_tutorial/tut_5_using_a_shared_or_static_library/libtest.cpp" "/home/nknight/Works/git/cmake_tutorial/tut_5_using_a_shared_or_static_library/build/CMakeFiles/libtest.dir/libtest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nknight/Works/git/cmake_tutorial/tut_4_building_a_static_library/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
